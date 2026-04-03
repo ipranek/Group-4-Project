e564dacd73664305edf08f408c1002d793cfe7de
İpek Öner <ipekoner2006@gmail.com>
2026-04-03 16:36:26 +0200

    Update narrative_structure.py

--- narrative_structure.py
+++ narrative_structure.py
@@ -29,6 +29,11 @@ topic_model = BERTopic(
 
 topics, probs = topic_model.fit_transform(docs)
 
+df_sample = df_sample.copy()
+df_sample["topic"] = topics #attach resulting index back to the texts
+doc_info = topic_model.get_document_info(docs) #for the centrality score of articles
+df_sample["probability"]  =doc_info["Probability"]
+
 # 5. Get the Narrative Summary
 topic_info = topic_model.get_topic_info()
 print("\n---  TOP 5 NARRATIVES IDENTIFIED ---")
@@ -40,3 +45,35 @@ topics_over_time = topic_model.topics_over_time(docs, timestamps, nr_bins=20)
 topics_over_time.to_csv("narrative_pulse_data.csv")
 
 print("\n SUCCESS! 'narrative_pulse_data.csv' is ready for your VAR model.")
+
+#NEWLY ADDED
+
+topic_dfs= {}
+
+for topic_id in df_sample["topic"].unique():
+    if topic_id == -1: #ignore the articles that couldnt be classified
+        continue
+    topic_dfs[topic_id] = df_sample[df_sample["topic"] == topic_id] #split dataset into mini datasets of 
+                                                                    #articles based on topics
+
+for topic_id, tdf in topic_dfs.items():##loop through each cluster
+    print(f"topic {topic_id}")
+    for i, row in tdf.head(5).iterrows(): #get first 5 article per topic
+        print(f"article {i}")
+        print(row["maintext"][:300])  #get the first 300 characters of first 5 artciles
+
+
+
+for topic_id in df_sample["topic"].unique():
+    if topic_id == -1: #ignore the articles that couldnt be classified
+        continue
+    print(f"topic {topic_id}")
+    #selects the highest centrality articles per topic
+    top_articles = (
+    df_sample[df_sample["topic"] == topic_id]
+    .sort_values("probability", ascending=False)
+    .head(5)
+)                                    
+    for i, row in top_articles.iterrows():
+        print("for n: \n")
+        print(row["maintext"][:300])
